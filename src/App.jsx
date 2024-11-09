3b15fe6b95fd88d0bc3b123bb8526cd290c7fdcc
arturchichorro <arturchichorro@gmail.com>
2024-11-09 16:31:45 +0000

    Current commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
 import StateTogether from './components/state-together-example'
 import MapComponent from './components/MapComponent.jsx';
-import { SessionManager } from 'react-together';
 
 function App() {
   return (
-    <div>
-      <h1>My Collaborative Map App</h1>
+    <div className="w-screen h-screen">
       <MapComponent />
       <StateTogether />
     </div>
--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,54 +1,128 @@
 import { useStateTogether } from 'react-together';
-import React, { useCallback, useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
-import CustomMarker from './CustomMarker'; // Import the custom marker component
+import CustomMarker from './CustomMarker';
 
 const mapContainerStyle = {
   width: '100%',
-  height: '400px',
+  height: '800px',
 };
 
 const center = {
-  lat: 40.748817,
-  lng: -73.985428,
+  lat: 38.7169,
+  lng: -9.1399,
 };
 
-function MapComponent() {
-  const [markers, setMarkers] = useStateTogether('map', []); // State to store marker positions
-  console.log('Markers:', markers);  // Debugging output for state
 
-  // Function to handle clicks on the map
+const mapOptions = { 
+  disableDefaultUI: true,  
+  zoomControl: false,            
+  mapTypeControl: false,       
+  streetViewControl: false,       
+  fullscreenControl: false,      
+  scaleControl: false,   
+  draggable: false, 
+  styles: [
+    {
+      featureType: "water",
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#d3d3d3",
+        },
+      ],
+    },
+    {
+      featureType: "landscape",
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#8ab8d1", 
+        },
+      ],
+    },
+    {
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#ffffff",
+        },
+      ],
+    },
+    {
+      featureType: "road",
+      elementType: "labels",
+      stylers: [
+        {
+          visibility: "off", 
+        },
+      ],
+    },
+    {
+      featureType: "poi",
+      elementType: "labels",
+      stylers: [
+        {
+          visibility: "off", 
+        },
+      ],
+    },
+    {
+      featureType: "poi.park",
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#8ab8d1",
+        },
+      ],
+    },
+    {
+      elementType: "labels",
+      stylers: [
+        {
+          visibility: "off",
+        },
+      ],
+
+  },
+  ],
+};
+
+
+function MapComponent() {
+  const [markers, setMarkers] = useStateTogether('map', []);
+  console.log('Markers:', markers);
+
   const handleMapClick = useCallback((event) => {
     const newMarker = {
       lat: event.latLng.lat(),
       lng: event.latLng.lng(),
     };
 
-    // Update state with the new marker
     setMarkers((prevMarkers) => [...prevMarkers, newMarker]);
   }, [setMarkers]);
 
   useEffect(() => {
-    console.log('Markers have been synced:', markers); // Check for syncing
+    console.log(markers);
   }, [markers]);
 
   return (
-    <div>
+    <div className="h-full w-full">
       <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_API_KEY}>
         <GoogleMap
           id="map"
           mapContainerStyle={mapContainerStyle}
           center={center}
           zoom={10}
-          onClick={handleMapClick} // Register the click event
+          onClick={handleMapClick}
+          options={mapOptions}
         >
           {markers.map((marker, index) => (
             <CustomMarker key={index} position={marker} />
           ))}
         </GoogleMap>
-      </LoadScript>
-
-      
+      </LoadScript> 
     </div>
   );
 }
