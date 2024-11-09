bd4eb7c913a4dc541e0b48a03c905699680f411c
arturchichorro <arturchichorro@gmail.com>
2024-11-09 11:29:09 +0000

    Added tailwind react-together

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ReactTogether } from 'react-together'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ReactTogether
+      sessionParams={{
+        appId: import.meta.env['VITE_APP_ID'],
+        apiKey: import.meta.env['VITE_API_KEY']
+      }}
+    >
+      <App />
+    </ReactTogether>
   </StrictMode>,
 )
