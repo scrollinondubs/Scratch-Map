9bb8844f1ca23b4d283535c49aefe7ad209a128a
arturchichorro <arturchichorro@gmail.com>
2024-11-10 03:18:48 +0000

    fixed qr code ui

--- src/components/generate-qr.jsx
+++ src/components/generate-qr.jsx
@@ -3,16 +3,16 @@ import QRCode from 'react-qr-code';
 import { useCreateRandomSession, useIsTogether, useJoinUrl } from 'react-together';
 import IconButton from './ui/button';
 import { RiQrCodeLine } from "react-icons/ri";
+import { FaRegClipboard } from "react-icons/fa";
 
 
 const SessionButton = () => {
   const [showQRCodeOverlay, setShowQRCodeOverlay] = useState(false);
+  const [buttonText, setButtonText] = useState('Copy to Clipboard');
 
   const isTogether = useIsTogether();
   const createRandomSession = useCreateRandomSession();
   const sessionId = useJoinUrl();
-
-  console.log(sessionId);
 
   const closeQRCodeOverlay = () => {
     setShowQRCodeOverlay(false);
@@ -26,6 +26,19 @@ const SessionButton = () => {
     }
   };
 
+  const copyToClipboard = () => {
+    if (sessionId) {
+      navigator.clipboard.writeText(sessionId).then(() => {
+        setButtonText('Copied!');
+        setTimeout(() => {
+          setButtonText('Copy to Clipboard');
+        }, 1000);
+      }).catch((err) => {
+        console.error('Failed to copy: ', err);
+      });
+    }
+  };
+
   return (
     <div>
       <IconButton 
@@ -36,12 +49,18 @@ const SessionButton = () => {
 
       {showQRCodeOverlay && (
         <div className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white p-6 rounded-lg text-center shadow-lg">
+          <div className="bg-white p-6 rounded-lg text-center shadow-lg flex flex-col gap-2">
             <QRCode value={sessionId || ''} />
-            <a href={sessionId} target="_blank">Url</a>
+            <button 
+              onClick={copyToClipboard}
+              className="px-4 py-2 bg-blue-700 rounded-lg hover:bg-blue-800 flex items-center justify-center gap-2 text-white"
+            >
+              <FaRegClipboard size={16} />
+              {buttonText}
+            </button>
             <button 
               onClick={closeQRCodeOverlay}
-              className="mt-4 px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition-all duration-300"
+              className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600"
             >
               Close QR
             </button>
@@ -53,4 +72,3 @@ const SessionButton = () => {
 };
 
 export default SessionButton;
-
