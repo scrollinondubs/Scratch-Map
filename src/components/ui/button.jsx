28180eb6c23d32071a94dec15372c24296c4157f
arturchichorro <arturchichorro@gmail.com>
2024-11-10 01:13:07 +0000

    updated bytton style

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 const Button = ({ children, ...props }) => {
   return (
     <button
-      className="p-2 font-semibold text-white bg-blue-500 rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+      className="p-2 font-semibold text-white bg-slate-700 rounded-lg shadow-md hover:bg-slate-800 focus:outline-none focus:ring-2 focus:ring-slate-700 focus:ring-offset-2"
       {...props}
     >
       {children}
