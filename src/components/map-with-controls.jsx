37bb0f5a16011d56ec74ce91dcd7898808e70647
arturchichorro <arturchichorro@gmail.com>
2024-11-10 01:15:44 +0000

    Added connected users

--- src/components/map-with-controls.jsx
+++ src/components/map-with-controls.jsx
@@ -2,7 +2,7 @@
 import MapComponent from './map-component.jsx';
 // import MapTest from './components/map-test.jsx';
 import ButtonQR from './generate-qr.jsx';
-// import { SessionManager } from 'react-together';
+import { ConnectedUsers } from 'react-together';
 import AddLocation from './add-location.jsx';
 import LeaveSession from './leave-session.jsx';
 import useMyStateTogether from '../hooks/useMyStateTogether.js';
@@ -29,6 +29,12 @@ function MapWithControls() {
                 <AddLocation location={location} setLocation={setLocation}/>
                 <LeaveSession setLocation={setLocation} setMarkers={setMarkers}/>
                 <ButtonQR />
+            </div>
+            <div className="flex justify-center items-center px-4 py-6 rounded-lg shadow-md">
+                <h3 className="text-lg font-semibold text-gray-100 mr-4">
+                    Users Connected:
+                </h3>
+                <ConnectedUsers />
             </div>
         </div>
     );
